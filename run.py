aa567881cbdfa56fcdd00f76a61b314cb30c9118
seasun2k <59240100+seasun2k@users.noreply.github.com>
2024-08-23 10:46:05 +0800

    Update run.py

--- run.py
+++ run.py
@@ -42,6 +42,7 @@ loginip = requests.get('https://api.ipify.org?format=json').json()['ip']
 content += f"本次登录用户共： {user_num} 个\n登录时间：{time}\n登录IP：{loginip}"
 
 push = os.getenv('PUSH')
+print("get from en sun")
 print(push)
 
 def mail_push(url):
@@ -95,4 +96,4 @@ elif push == "serverchan":
 elif push == "telegram":
     telegram_push(content)
 else:
-    print("推送失败，推送参数设置错误",push)
+    print("推送失败，推送参数设置错误")
